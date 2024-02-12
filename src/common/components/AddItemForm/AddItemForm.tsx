84734591bd4804c9abbbe84611f4a1518f967cde
vustinchdev <vustinchdev@gmail.com>
2024-02-12 17:12:04 +0300

    feat: add AddItemForm

---
+++ src/common/components/AddItemForm/AddItemForm.tsx
@@ -0,0 +1,61 @@
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { BaseResponse } from "common/types";
+
+type Props = {
+  addItem: (newTitle: string) => Promise<any>;
+  disabled?: boolean;
+};
+
+export const AddItemForm = ({ addItem, disabled = false }: Props) => {
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState<null | string>(null);
+
+  const addItemHandler = () => {
+    let newTitle = title.trim();
+    if (newTitle !== "") {
+      addItem(title)
+        .then(() => {
+          setTitle("");
+        })
+        .catch((e: BaseResponse) => {
+          if (e?.resultCode) {
+            setError(e.messages[0]);
+          }
+        });
+    } else {
+      setError("Title is required");
+    }
+  };
+  const ChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+  const addItemOnEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (error !== null) {
+      setError(null);
+    }
+    if (e.key === "Enter") {
+      addItemHandler();
+    }
+  };
+  return (
+    <div>
+      <TextField
+        error={!!error}
+        id="standard-basic"
+        label={error ? "Title" : "type smth..."}
+        size="small"
+        variant="standard"
+        value={title}
+        onChange={ChangeTitleHandler}
+        helperText={error}
+        onKeyDown={addItemOnEnterHandler}
+      />
+      <IconButton onClick={addItemHandler}>
+        <AddTaskIcon />
+      </IconButton>
+    </div>
+  );
+};
