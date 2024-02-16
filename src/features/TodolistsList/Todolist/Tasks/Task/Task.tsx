9beaf469812f2d6222ba4f796c275b37444b6631
vustinchdev <vustinchdev@gmail.com>
2024-02-16 12:42:00 +0300

    feat: implement delete task from todolist

--- src/features/TodolistsList/Todolist/Tasks/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Tasks/Task/Task.tsx
@@ -2,6 +2,8 @@ import { EditableSpan } from "common/components/EditableSpan/EditableSpan";
 import { useAppDispatch } from "common/hooks";
 import { TaskResponse } from "features/TodolistsList/api/tasks/tasksApi.types";
 import { tasksActions } from "features/TodolistsList/model/tasks/tasksSlice";
+import IconButton from "@mui/material/IconButton";
+import BackspaceIcon from "@mui/icons-material/Backspace";
 
 type Props = {
   todolistId: string;
@@ -20,10 +22,16 @@ export const Task = ({ todolistId, task }: Props) => {
       }),
     );
   };
+  const deleteTaskHandler = () => {
+    dispatch(tasksActions.deleteTask({ todolistId, taskId: task.id }));
+  };
 
   return (
     <li>
       <EditableSpan titleValue={task.title} onChange={onChangeTaskHandler} />
+      <IconButton aria-label="delete" onClick={deleteTaskHandler}>
+        <BackspaceIcon />
+      </IconButton>
     </li>
   );
 };
