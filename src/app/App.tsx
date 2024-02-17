824fec51fff4142c2dfddec26de5ace6887bcfe9
vustinchdev <vustinchdev@gmail.com>
2024-02-17 11:01:17 +0300

    feat: add ButtonAppBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { TodolistsList } from "features/TodolistsList/TodolistsList";
 import "./App.css";
 import Container from "@mui/material/Container";
+import { ButtonAppBar } from "common/components/ButtonAppBar/ButtonAppBar";
 
 function App() {
   return (
     <div>
+      <ButtonAppBar />
       <Container>
         <TodolistsList />
       </Container>
