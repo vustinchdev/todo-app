46584674563ed45e2d8b1c013d3df1f68d705c1f
vustinchdev <vustinchdev@gmail.com>
2024-02-17 10:34:53 +0300

    feat: implement change task status

--- src/features/TodolistsList/Todolist/Tasks/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Tasks/Task/Task.tsx
@@ -4,6 +4,9 @@ import { TaskResponse } from "features/TodolistsList/api/tasks/tasksApi.types";
 import { tasksActions } from "features/TodolistsList/model/tasks/tasksSlice";
 import IconButton from "@mui/material/IconButton";
 import BackspaceIcon from "@mui/icons-material/Backspace";
+import Checkbox from "@mui/material/Checkbox";
+import { ChangeEvent } from "react";
+import { TaskStatuses } from "common/enums";
 
 type Props = {
   todolistId: string;
@@ -25,9 +28,25 @@ export const Task = ({ todolistId, task }: Props) => {
   const deleteTaskHandler = () => {
     dispatch(tasksActions.deleteTask({ todolistId, taskId: task.id }));
   };
+  const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const status = e.currentTarget.checked
+      ? TaskStatuses.Completed
+      : TaskStatuses.New;
+    dispatch(
+      tasksActions.updateTask({
+        todolistId,
+        taskId: task.id,
+        domainModel: { status },
+      }),
+    );
+  };
 
   return (
     <li>
+      <Checkbox
+        checked={task.status === TaskStatuses.Completed}
+        onChange={changeTaskStatusHandler}
+      />
       <EditableSpan titleValue={task.title} onChange={onChangeTaskHandler} />
       <IconButton aria-label="delete" onClick={deleteTaskHandler}>
         <BackspaceIcon />
